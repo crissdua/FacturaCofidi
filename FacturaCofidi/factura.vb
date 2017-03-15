3fa84df383c7f8d81853afcfe7d85c2835504875
crissdua <crissdua@hotmail.com>
2017-03-15 17:30:00 -0600

    Base
    Inicial

---
+++ FacturaCofidi/factura.vb
@@ -0,0 +1,262 @@
+﻿Imports System.Xml
+
+Public Class factura
+    Private WithEvents SBO_Application As SAPbouiCOM.Application
+    Private oCompany As SAPbobsCOM.Company
+    Private oBusinessForm As SAPbouiCOM.Form
+    Public oFilters As SAPbouiCOM.EventFilters
+    Public oFilter As SAPbouiCOM.EventFilter
+    Private oMatrix As SAPbouiCOM.Matrix        ' Global variable to handle matrixes
+
+    Private AddStarted As Boolean                ' Flag that indicates "Add" process started
+
+    Private RedFlag As Boolean                   ' RedFlag when true indicates an error during "Add" process
+
+
+#Region "Single Sign On"
+
+    Private Sub SetApplication()
+
+
+        AddStarted = False
+
+        RedFlag = False
+
+        '*******************************************************************
+
+        '// Use an SboGuiApi object to establish connection
+
+        '// with the SAP Business One application and return an
+
+        '// initialized application object
+
+        '*******************************************************************
+        Try
+            Dim SboGuiApi As SAPbouiCOM.SboGuiApi
+
+            Dim sConnectionString As String
+
+            SboGuiApi = New SAPbouiCOM.SboGuiApi
+
+            '// by following the steps specified above, the following
+
+            '// statement should be sufficient for either development or run mode
+            If Environment.GetCommandLineArgs.Length > 1 Then
+                sConnectionString = Environment.GetCommandLineArgs.GetValue(1)
+            Else
+                sConnectionString = Environment.GetCommandLineArgs.GetValue(0)
+            End If
+
+            'sConnectionString = Environment.GetCommandLineArgs.GetValue(1) '"0030002C0030002C00530041005000420044005F00440061007400650076002C0050004C006F006D0056004900490056"
+
+            '// connect to a running SBO Application
+
+            SboGuiApi.Connect(sConnectionString)
+
+            '// get an initialized application object
+
+            SBO_Application = SboGuiApi.GetApplication()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+
+
+    End Sub
+
+    Private Function SetConnectionContext() As Integer
+
+        Dim sCookie As String
+
+        Dim sConnectionContext As String
+
+        Dim lRetCode As Integer
+
+        Try
+
+            '// First initialize the Company object
+
+            oCompany = New SAPbobsCOM.Company
+
+            '// Acquire the connection context cookie from the DI API.
+
+            sCookie = oCompany.GetContextCookie
+
+            '// Retrieve the connection context string from the UI API using the
+
+            '// acquired cookie.
+
+            sConnectionContext = SBO_Application.Company.GetConnectionContext(sCookie)
+
+            '// before setting the SBO Login Context make sure the company is not
+
+            '// connected
+
+            If oCompany.Connected = True Then
+
+                oCompany.Disconnect()
+
+            End If
+
+            '// Set the connection context information to the DI API.
+
+            SetConnectionContext = oCompany.SetSboLoginContext(sConnectionContext)
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Function
+
+    Private Function ConnectToCompany() As Integer
+
+        '// Establish the connection to the company database.
+
+        ConnectToCompany = oCompany.Connect
+
+    End Function
+
+
+    Private Sub Class_Init()
+        Try
+            
+            '//*************************************************************
+
+            '// set SBO_Application with an initialized application object
+
+            '//*************************************************************
+
+            SetApplication()
+
+            '//*************************************************************
+
+            '// Set The Connection Context
+
+            '//*************************************************************
+
+            If Not SetConnectionContext() = 0 Then
+
+                SBO_Application.MessageBox("Failed setting a connection to DI API")
+
+                End ' Terminating the Add-On Application
+
+            End If
+
+            '//*************************************************************
+
+            '// Connect To The Company Data Base
+
+            '//*************************************************************
+
+            If Not ConnectToCompany() = 0 Then
+
+                SBO_Application.MessageBox("Failed connecting to the company's Data Base")
+
+                End ' Terminating the Add-On Application
+
+            End If
+
+            '//*************************************************************
+
+            '// send an "hello world" message
+
+            '//*************************************************************
+
+            SBO_Application.SetStatusBarMessage("DI Connected To: " & oCompany.CompanyName & vbNewLine & "Add-on is loaded", SAPbouiCOM.BoMessageTime.bmt_Short, False)
+            'SetNewItems()
+
+            'SetNewTax("01", "512 0% a 22 % pago al exterior", SAPbobsCOM.WithholdingTaxCodeCategoryEnum.wtcc_Invoice, SAPbobsCOM.WithholdingTaxCodeBaseTypeEnum.wtcbt_Net, 100, "512", "1-1-010-10-000")
+            'SetNewTax("02", "513 0% a 22 % pago al exterior", SAPbobsCOM.WithholdingTaxCodeCategoryEnum.wtcc_Invoice, SAPbobsCOM.WithholdingTaxCodeBaseTypeEnum.wtcbt_Net, 100, "513", "1-1-010-10-000")
+            'SetNewTax("03", "513A 0% a 22 % pago al exterior", SAPbobsCOM.WithholdingTaxCodeCategoryEnum.wtcc_Invoice, SAPbobsCOM.WithholdingTaxCodeBaseTypeEnum.wtcbt_Net, 100, "513A", "_SYS00000000128")
+            'SetNewTax("04", "514 0% a 22 % pago al exterior", SAPbobsCOM.WithholdingTaxCodeCategoryEnum.wtcc_Invoice, SAPbobsCOM.WithholdingTaxCodeBaseTypeEnum.wtcbt_Net, 100, "514", "_SYS00000000128")
+
+            ' Dim wsCodifi As New WSCofidi.Service1
+            'Dim oDocEnviar As New XmlDocument
+            ' oDocEnviar.Load(Application.StartupPath & "\(F) No.18.xml")
+            'Dim respuesta = wsCodifi.GeneraDTE("00000REM01", "", "REMISA", "12345678", oDocEnviar.InnerText, "02", "")
+            'UDT_UF.Company = Me.oCompany
+            'cargarInicial(oCompany, SBO_Application)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+    End Sub
+
+#End Region
+
+
+
+    Public Sub New()
+
+        MyBase.New()
+
+        Class_Init()
+
+        'AddMenuItems()
+
+        SetFilters()
+
+
+    End Sub
+
+    Private Sub SetFilters()
+
+        '// Create a new EventFilters object
+
+        oFilters = New SAPbouiCOM.EventFilters
+
+
+
+        '// add an event type to the container
+
+        '// this method returns an EventFilter object
+
+        oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
+        oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_DATA_ADD)
+        oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_DATA_UPDATE)
+        oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_DATA_DELETE)
+        oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_LOAD)
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_MENU_CLICK)
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_CLICK)
+        'oFilter = oFilter.Add(SAPbouiCOM.BoEventTypes.et_DOUBLE_CLICK)
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_COMBO_SELECT)
+
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_VALIDATE)
+
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_LOST_FOCUS)
+
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_KEY_DOWN)
+
+        'oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_MENU_CLICK)
+
+        ' oFilter = oFilters.Add(SAPbouiCOM.BoEventTypes.et_DOUBLE_CLICK)
+        ' oFilter.AddEx("60006") 'Quotation Form
+
+
+
+        '// assign the form type on which the event would be processed
+
+        ' oFilter.AddEx("134") 'Quotation Form
+        'oFilter.AddEx("141")
+        'oFilter.AddEx("-141")
+        oFilter.AddEx("133")
+        
+        SBO_Application.SetFilter(oFilters)
+
+    End Sub
+
+    Private Sub SBO_Application_DATAEVENT(ByRef pVal As SAPbouiCOM.BusinessObjectInfo, ByRef BubbleEvent As Boolean) Handles SBO_Application.FormDataEvent
+        Try
+            If pVal.FormTypeEx = "133" And pVal.BeforeAction = True And pVal.EventType = SAPbouiCOM.BoEventTypes.et_FORM_DATA_ADD And pVal.ActionSuccess = True Then
+                MessageBox.Show("ENTRO AQUI CUANDO AGREGO DATA")
+            End If
+
+        Catch ex As Exception
+            Me.SBO_Application.SetStatusBarMessage(ex.Message, SAPbouiCOM.BoMessageTime.bmt_Medium, True)
+        End Try
+
+
+    End Sub
+End Class
